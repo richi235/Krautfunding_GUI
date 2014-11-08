a6f1ad9d74de921fc3a2be61691792b876e941a3
Richard Sailer <richard@weltraumpflege.org>
2014-11-08 05:09:33 +0100

    reformated DBDesign.pm with perltidy for readability

--- DBDesign.pm
+++ DBDesign.pm
@@ -9,112 +9,106 @@ use ADBGUI::Tools qw(Log);
 our @ISA;
 
 BEGIN {
-   use Exporter;
-   our @ISA = qw(Exporter);
-   our @EXPORT = qw//;
+    use Exporter;
+    our @ISA    = qw(Exporter);
+    our @EXPORT = qw//;
 }
 
 sub new {
-   my $proto = shift;
-   my $class = ref($proto) || $proto;
-   my $self = shift;
-   my $parent = $self ? ref($self) : "";
-   @ISA = ($parent) if $parent;
-   $self = $self ? $self : {};
-   bless ($self, $class);
-   return $self;
+    my $proto  = shift;
+    my $class  = ref($proto) || $proto;
+    my $self   = shift;
+    my $parent = $self ? ref($self) : "";
+    @ISA = ($parent) if $parent;
+    $self = $self ? $self : {};
+    bless( $self, $class );
+    return $self;
 }
 
 sub getDB {
-   my $self = shift;
-   my $DB = $self->SUPER::getDB() || {};
+    my $self = shift;
+    my $DB = $self->SUPER::getDB() || {};
 
-   $DB->{tables}->{"projects"} =
-   {
-      rights => $RIGHTS,
-      dbuser => $DBUSER,
-      primarykey => ["id"],
+    $DB->{tables}->{"projects"} = {
+        rights     => $RIGHTS,
+        dbuser     => $DBUSER,
+        primarykey => ["id"],
 
-      dblclick => "doubleclick_on_projects",
-      label => "Projekte",
-      order => 1,
-      idcolumnname => "id",
+        dblclick     => "doubleclick_on_projects",
+        label        => "Projekte",
+        order        => 1,
+        idcolumnname => "id",
 
-      columns => {
-         "id" => {
-            type => $UNIQIDCOLUMNNAME,
-         },
-         "Name" => {
-            showInSelect => 1,
-            type => "text",
-            order => 1,
-         },
-         "cost" => {
-            label => "Kosten",
-            type => "double",
-            order => 3,
-         },
-         "contact_person_id" => {
-            linkto => "users", 
-            label => "Zustaendige Kontaktperson",
-            type => "longnumber",
-            order => 4,
-         },
-         "amount_missing" => {
-            label => "Fehlender Betrag",
-            type => "double",
-            order => 2,
-            syntaxcheck => '^\-?\d+(\.\d+)?$',
-         },
-         "deleted" => {
-            type => $DELETEDCOLUMNNAME,
-            order => 5,
-         },
-      }
-   };
-   $DB->{tables}->{"transactions"} =
-   {
-      rights => $RIGHTS,
-      dbuser => $DBUSER,
-      primarykey => ["id"],
+        columns => {
+            "id"   => { type => $UNIQIDCOLUMNNAME, },
+            "Name" => {
+                showInSelect => 1,
+                type         => "text",
+                order        => 1,
+            },
+            "cost" => {
+                label => "Kosten",
+                type  => "double",
+                order => 3,
+            },
+            "contact_person_id" => {
+                linkto => "users",
+                label  => "Zustaendige Kontaktperson",
+                type   => "longnumber",
+                order  => 4,
+            },
+            "amount_missing" => {
+                label       => "Fehlender Betrag",
+                type        => "double",
+                order       => 2,
+                syntaxcheck => '^\-?\d+(\.\d+)?$',
+            },
+            "deleted" => {
+                type  => $DELETEDCOLUMNNAME,
+                order => 5,
+            },
+        }
+    };
+    $DB->{tables}->{"transactions"} = {
+        rights     => $RIGHTS,
+        dbuser     => $DBUSER,
+        primarykey => ["id"],
 
-      label => "Spenden", 
-      order => 2,
-      realdelete => 1,
-      idcolumnname => "id",
+        label        => "Spenden",
+        order        => 2,
+        realdelete   => 1,
+        idcolumnname => "id",
 
-      columns => {
-         "id" => {
-            type => $UNIQIDCOLUMNNAME,
-         },
-         "project_id" => {
-            label => "Projekt",
-            linkto => "projects",
-            type => "number",
-            order => 2,
-         },
-         "user_id" => {
-            readonly => 1,
-            label => "Spender",
-            showInSelect => 1,
-            linkto => "users",
-            type => "longnumber",
-            order => 1,
-         },
-         "value" => {
-            label => "Betrag",
-            showInSelect => 1,
-            type => "double",
-            order => 3,
-         },
-         "deleted" => {
-            type => $DELETEDCOLUMNNAME,
-            order => 77,
-         },
-      }
-   };
+        columns => {
+            "id"         => { type => $UNIQIDCOLUMNNAME, },
+            "project_id" => {
+                label  => "Projekt",
+                linkto => "projects",
+                type   => "number",
+                order  => 2,
+            },
+            "user_id" => {
+                readonly     => 1,
+                label        => "Spender",
+                showInSelect => 1,
+                linkto       => "users",
+                type         => "longnumber",
+                order        => 1,
+            },
+            "value" => {
+                label        => "Betrag",
+                showInSelect => 1,
+                type         => "double",
+                order        => 3,
+            },
+            "deleted" => {
+                type  => $DELETEDCOLUMNNAME,
+                order => 77,
+            },
+        }
+    };
 
-   return $DB;
+    return $DB;
 
 }
 
